bae69a487e89ea67c9ceb652990146cedf4e9960
Andrew <klyzub102991@aol.com>
2025-01-20 11:33:08 -0500

    1/20/25

--- app.js
+++ app.js
@@ -17,28 +17,38 @@ function toggleContrast() {
     }
 }
 
-function contact(event) {
+// function contact(event) {
+//     event.preventDefault();
+//     const loading = document.querySelector('.modal__overlay--loading')
+//     const success = document.querySelector('.modal__overlay--success')
+//     loading.classlist += ' .modal__overlay--visible'
+//     emailjs
+//         .sendform(
+//             'service_flayun8',
+//             'template_lhhfis9',
+//             event.target,
+//             'p-H7HsXoSmEKGe6_l'
+//     ).then(() => {
+//         loading.classList.remove('modal__overlay--visible')
+//         success.classList += ' modal__overlay--visible'
+//     }).catch(() => {
+//         loading.classList.remove('modal__overlay--visible')
+//         alert(
+//             "The email service is teporarily unavalible. Please contact me directly at andrewkyndheart@gmail.com"
+//         )
+//     })
+// }
+function contact(event){
     event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classlist += ' modal__overlay--visible'
-    emailjs
-        .sendform(
-            'service_flayun8',
-            'template_lhhfis9',
-            event.target,
-            'p-H7HsXoSmEKGe6_l'
-    ).then(() => {
-        loading.classList.remove('modal__overlay--visible')
-        success.classList += ' modal__overlay--visible'
-    }).catch(() => {
-        loading.classList.remove('modal__overlay--visible')
-        alert(
-            "The email service is teporarily unavalible. Please contact me directly at andrewkyndheart@gmail.com"
-        )
-    })
+    const loading = document.querySelector(" .modal__overlay--loading")
+    const success = document.querySelector(" .modal__overlay--success")
+    loading.classList += " .modal__overlay--visible"
+    setTimeout(() => {
+        loading.classList.remove(".+modal__overlay--visible")
+        success.classList.remove(".modal__overlay--visible")
+        console.log('it worked')
+    }, 1000);
 }
-
 
 function toggleModal() {
     if (isModalOpen) {
