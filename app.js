24dad39e68a4313a65750bb0078a1d92496fbbe3
Andrew <klyzub102991@aol.com>
2025-01-21 10:54:28 -0500

    email fix

--- app.js
+++ app.js
@@ -9,45 +9,32 @@ function toggleContrast() {
     contrastToggle = !contrastToggle
     if (contrastToggle) {
         return document.body.classList += ' dark-theme'
-        console.log("dark")
     }
     else {
         return document.body.classList.remove ('dark-theme')
-        console.log("light")
     }
 }
 
-// function contact(event) {
-//     event.preventDefault();
-//     const loading = document.querySelector('.modal__overlay--loading')
-//     const success = document.querySelector('.modal__overlay--success')
-//     loading.classlist += ' .modal__overlay--visible'
-//     emailjs
-//         .sendform(
-//             'service_flayun8',
-//             'template_lhhfis9',
-//             event.target,
-//             'p-H7HsXoSmEKGe6_l'
-//     ).then(() => {
-//         loading.classList.remove('modal__overlay--visible')
-//         success.classList += ' modal__overlay--visible'
-//     }).catch(() => {
-//         loading.classList.remove('modal__overlay--visible')
-//         alert(
-//             "The email service is teporarily unavalible. Please contact me directly at andrewkyndheart@gmail.com"
-//         )
-//     })
-// }
-function contact(event){
+function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector(" .modal__overlay--loading")
-    const success = document.querySelector(" .modal__overlay--success")
-    loading.classList += " .modal__overlay--visible"
-    setTimeout(() => {
-        loading.classList.remove(".+modal__overlay--visible")
-        success.classList.remove(".modal__overlay--visible")
-        console.log('it worked')
-    }, 1000);
+    const loading = document.querySelector(' .modal__overlay--loading')
+    const success = document.querySelector(' .modal__overlay--success')
+    loading.classlist += ' modal__overlay--visible'
+    emailjs
+        .sendForm(
+            'service_flayun8',
+            'template_lhhfis9',
+            event.target,
+            'p-H7HsXoSmEKGe6_l'
+    ).then(() => {
+        loading.classList.remove('modal__overlay--visible')
+        success.classList += ' modal__overlay--visible'
+    }).catch(() => {
+        loading.classList.remove('modal__overlay--visible')
+        alert(
+            "The email service is teporarily unavalible. Please contact me directly at andrewkyndheart@gmail.com"
+        )
+    })
 }
 
 function toggleModal() {
