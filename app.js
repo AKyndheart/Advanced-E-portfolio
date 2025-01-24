0de0d622fe05a574f21caafa66b7e9bdbc7e3634
Andrew <klyzub102991@aol.com>
2025-01-24 10:33:34 -0500

    Background Shapes

--- app.js
+++ app.js
@@ -3,7 +3,20 @@
 // p-H7HsXoSmEKGe6_l
 
 let isModalOpen = false
-let contrastToggle =false
+let contrastToggle = false
+const scaleFactor= 1/20
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll('.shape')
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0
+        const boolInt = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x *boolInt}px, ${y * boolInt}px)`
+    }
+}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle
